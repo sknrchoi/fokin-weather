93ff63589d58e2db904a320a1ac8ad058d718213
sknrchoi <seulkinara@gmail.com>
2020-11-06 17:02:27 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Loading from "./Loading";
 
 export default function App() {
   return (
     // bridge for native core
-    <View style={styles.container}>
-      <View style={styles.yelloView}></View>
-      <View style={styles.blueView}></View>
-    </View>
+    <Loading/>
   );
 }
-
-// stylesheet api
-const styles = StyleSheet.create({
-  // css for view content
-  container: {
-    flex: 1,
-  },
-  // css for inner view content
-  yelloView: {
-    flex: 1,
-    backgroundColor: "yello"
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: "blue"
-  }
-});
---
+++ Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the weather</Text>
+        </View>
+    );
+}
+
+// stylesheet api
+const styles = StyleSheet.create({
+    // css for view content
+    container: {
+      flex: 1,
+      justifyContent: "flex-end",
+      paddingHorizontal: 30,
+      paddingVertical: 100,
+      backgroundColor: "#FDF6AA"
+    },
+    // css for inner view content
+    text: {
+        color: "#2c2c2c",
+        fontSize: 30
+    }
+  });
